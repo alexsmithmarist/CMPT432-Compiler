d51ddbe763029de44487d82c553864b97d7d77e6
alexsmithmarist <alex.smith1@marist.edu>
2019-01-31 10:16:55 -0500

    Implemented ArrayList to store Tokens created from input

--- prj/1/Compiler_prj1.java
+++ prj/1/Compiler_prj1.java
@@ -1,25 +1,38 @@
 //Scanner to read input
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Compiler_prj1 {
   
-   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
     
-    //Simple testing of Token class 
-    Token test = new Token ("L_Paren", 1, 2);
-    System.out.println (test.type);
-    System.out.println (test.lineNum);
-    System.out.println (test.indexNum);
+    Scanner input = new Scanner(System.in);
+    String line;
+    int lineNum = 0;
+    int indexNum = 0;
+    int tokenNum = 0;
+    ArrayList<Token> list = new ArrayList<Token>();
     
     //Simple testing of Making a token sequence and printing it
-    Token[] stream = new Token[1];
-    stream[0] = test;
     
-    for (int i = 0; i < stream.length; i++) {
-      if(stream[i].type == "L_Paren"){
-        System.out.println("L_PAREN DETECTED AT " +stream[i].lineNum+ ":" +stream[i].indexNum);  
+    while(input.hasNext()){
+      line = input.nextLine();
+      lineNum = lineNum + 1;
+      for(int i = 0; i != line.length(); i++){
+        indexNum = indexNum + 1;
+        
+        //INSERT MORE EFFICIENT CHECKING CODE HERE!!!!
+        
+        if(line.charAt(i) == '('){
+          list.add(new Token("L_Paren", lineNum, indexNum));
+          System.out.println(list.get(tokenNum).type + " DISCOVERED AT LINE " + list.get(tokenNum).lineNum + ", INDEX " +list.get(tokenNum).indexNum);
+          tokenNum = tokenNum + 1;
+        }
+        
+        //REPLACE CHECKING CODE ABOVE!!!!
+          
       }
+      indexNum = 0;
     }
-   }
+  }
 }
