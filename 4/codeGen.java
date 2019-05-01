8b68babc312cdcafa3b8df4169408cadb367c725
alexsmithmarist <alex.smith1@marist.edu>
2019-04-30 20:36:43 -0400

    Added basic opCodes for initialization

--- 4/codeGen.java
+++ 4/codeGen.java
@@ -5,8 +5,49 @@ public class codeGen{
   ArrayList<sVar> staticTable = new ArrayList<sVar>();
   ArrayList<jVar> jumpTable = new ArrayList<jVar>();
   int curPos = 0;
+  int staticNum = 0;
+  int jumpNum = 0;
+  boolean stop = false;
     
-  public codeGen(){};
+  public codeGen(){
+    for(int i = 0; i < 255; i++){
+      opCode[i] = "00";
+    }
+  };
+    
+  public void generate (CSTNode node){
+    if(node.tokType.equals("Var Decl")){
+      opCode[curPos] = "A9";
+      curPos = curPos + 1;
+      
+      opCode[curPos] = "00";
+      curPos = curPos + 1;
+        
+      String tempName = "T" + staticNum;
+      sVar tempSpot = new sVar(tempName, node.children.get(1).name, staticNum);
+        
+      opCode[curPos] = tempName;
+      curPos = curPos +1;
+        
+      opCode[curPos] = "XX";
+      curPos = curPos+1;
+      staticNum = staticNum+1;
+      
+    }
+      
+      
+      
+      
+      
+      
+    if(node.children.size() != 0 && !stop){
+      for (int j = 0; j < node.children.size(); j++){
+        generate(node.children.get(j));
+      }
+    }
+  }
+    
+  
   
 }
 
